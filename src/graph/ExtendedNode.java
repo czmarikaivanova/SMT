64c61330bf62afce3aff4089aca9d86d63fcc6c4
miv022 <marikaivanova@seznam.cz>
2016-08-04 13:22:09 +0200

    getters and setters for extended node

--- src/graph/ExtendedNode.java
+++ src/graph/ExtendedNode.java
@@ -2,4 +2,44 @@ package graph;
 
 public class ExtendedNode {
 
+	private Node origU;
+	private Node origV;
+	private double weight;
+	private int id;
+	
+	public ExtendedNode(Node origU, Node origV, double weight, int id) {
+		this.origU = origU;
+		this.origV = origV;
+		this.weight = weight;
+		this.id = id;
+	}
+	
+	/**
+	 * @return the origU
+	 */
+	public Node getOrigU() {
+		return origU;
+	}
+
+	/**
+	 * @return the origV
+	 */
+	public Node getOrigV() {
+		return origV;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
 }
